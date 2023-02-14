61ae4d30aeedea4270d2a651ccaab582d4740685
akashswfu <splashakash99@gmail.com>
2023-02-14 16:32:01 +0600

    projectdone

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,26 +3,38 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
+    withdrawField.value = '';
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
+
+
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    const currentWithdrawTotal = newWithdrawAmount+previousWithdrawTotal;
-    withdrawTotalElement.innerText=currentWithdrawTotal;
+    
 
 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    withdrawField.value = '';
+
+    if(newWithdrawAmount>previousBalanceTotal){
+        alert('Baap er bank a eto taka nai');
+        return;
+    }
+    const currentWithdrawTotal = newWithdrawAmount+previousWithdrawTotal;
+    withdrawTotalElement.innerText=currentWithdrawTotal;
+
+
     const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = currentBalanceTotal;
 
 
 
-
-
-
-
-
-    withdrawField.value = '';
 
 })
